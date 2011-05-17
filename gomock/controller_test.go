ba54ae993e7858a2e8d19d26e5c925d4610cb21c
David Symonds <dsymonds@golang.org>
2011-05-17 13:00:51 +1000

    Fix package names of tests.

--- gomock/controller_test.go
+++ gomock/controller_test.go
@@ -12,7 +12,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-package controller_test
+package gomock_test
 
 import (
 	"fmt"
--- gomock/matchers_test.go
+++ gomock/matchers_test.go
@@ -12,7 +12,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-package matchers_test
+package gomock_test
 
 import (
 	"testing"
