5cd493656ffd1ccc6ed90254e222154dd74aa242
Siva Kalva <sivakk@uber.com>
2017-10-04 15:49:37 -0700

    Dynamic return values.
    Dynamically return values based on the processing on the inputs. Add unit test as well.

--- gomock/controller.go
+++ gomock/controller.go
@@ -148,7 +148,7 @@ func (ctrl *Controller) Call(receiver interface{}, method string, args ...interf
 		ctrl.expectedCalls.Remove(preReqCall)
 	}
 
-	rets, action := expected.call(args)
+	action := expected.call(args)
 	if expected.exhausted() {
 		ctrl.expectedCalls.Remove(expected)
 	}
@@ -163,7 +163,7 @@ func (ctrl *Controller) Call(receiver interface{}, method string, args ...interf
 		action()
 	}
 
-	return rets
+	return expected.rets
 }
 
 func (ctrl *Controller) Finish() {
