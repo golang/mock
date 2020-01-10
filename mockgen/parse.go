099563292e166690d541d839c000a68adac51be0
Cody Oss <codyoss@google.com>
2020-01-10 13:55:50 -0700

    fix formatting

--- mockgen/parse.go
+++ mockgen/parse.go
@@ -187,7 +187,7 @@ func (p *fileParser) parseFile(importPath string, file *ast.File) (*model.Packag
 	}
 	return &model.Package{
 		Name:       file.Name.String(),
-		PkgPath: importPath,
+		PkgPath:    importPath,
 		Interfaces: is,
 		DotImports: dotImports,
 	}, nil
