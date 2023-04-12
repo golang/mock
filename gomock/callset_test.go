9e0829c0a0eb740afbe123bb4022bece6a0a579e
Florian Loch <florian.loch@exaring.de>
2023-04-12 15:24:11 +0200

    refactor: make expected map safe for concurrent usage

--- gomock/callset_test.go
+++ gomock/callset_test.go
@@ -69,6 +69,9 @@ func TestCallSetRemove(t *testing.T) {
 		}
 	}
 
+	cs.expectedMu.Lock()
+	defer cs.expectedMu.Unlock()
+
 	for _, c := range ourCalls {
 		validateOrder(cs.expected[callSetKey{receiver, method}])
 		cs.Remove(c)
